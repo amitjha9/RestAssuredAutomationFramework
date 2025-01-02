e7e5dc26022eb7b312cca958c72de8dea68b4a5f
Amit Kumar Jha <amitkumarjha@Amits-MacBook-Pro.local>
2025-01-02 14:35:07 +0530

    Added code for one GET call

--- src/test/java/airlines/GetAirlineByAirlineIDTest.java
+++ src/test/java/airlines/GetAirlineByAirlineIDTest.java
@@ -1,19 +1,27 @@
 package airlines;
 
+
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class GetAirlineByAirlineIDTest extends AirlinesApi{
 
 
-    @Test
+    @Test(description = "This test is to get the Airline details by Airline ID")
     public void getAirlineByAirlineIDTest() throws IOException {
-        String airLineID = "87dacc97-3c9a-4dfe-8719-062153eabca9";
-        Response response = getAirLineByAirlineID(airLineID);
+
+        Map<String, Object> getData = AirlinesBaseTest.getAirlineApiEndPoint();
+        String endPoint = (String) getData.get("GetAirlineByAirlineID");
+
+        JSONObject jsonInputRequest = AirlinesBaseTest.getJsonInputRequestBody(this.getClass().getSimpleName());
+        JSONObject jsonGetAirlineById = (JSONObject) jsonInputRequest.get("GetAirlineByAirlineIDTestData");
+        endPoint = endPoint + jsonGetAirlineById.get("airlineId");
+        Response response = getAirLines(endPoint);
         Assert.assertEquals(response.getStatusCode(), 200);
     }
-
 }
